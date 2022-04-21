5d9f75837d6ea9965a41adcd55434c07925c8826
Nuno Oliveira <nfnoliveira@hotmail.com>
2022-04-21 15:02:38 +0100

    Add Navbar component and assets

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { AnimatePresence, motion } from "framer-motion";
 
 import "./Navbar.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "skills", "work", "contact"].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        <AnimatePresence>
+          {toggle && (
+            <motion.div
+              initial={{ width: 0 }}
+              animate={{ width: 300 }}
+              exit={{ width: 0, transition: { delay: 0.3, duration: 0.3 } }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                {["home", "about", "skills", "work", "contact"].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,25 @@
+import bg from "../assets/app_bg_2.png";
+import css from "../assets/css.png";
+import email from "../assets/email.png";
+import html from "../assets/html.png";
+import line_waves from "../assets/Line_waves_4k.svg";
+import line_waves_2 from "../assets/Line_waves_2_4k.svg";
+import line_waves_contact from "../assets/Line_waves_contact_4k.svg";
+import logo from "../assets/logo.png";
+import mobile from "../assets/mobile.png";
+import profile from "../assets/profile.png";
+import react from "../assets/react.png";
+
+export default {
+  bg,
+  css,
+  email,
+  html,
+  line_waves,
+  line_waves_2,
+  line_waves_contact,
+  logo,
+  mobile,
+  profile,
+  react,
+};
